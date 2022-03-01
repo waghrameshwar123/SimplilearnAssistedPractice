e50edfc5166fa63e85823aa023f4d4983323a1c0
Rameshwar Waghmare <waghrameshwar123@gmail.com>
2022-03-01 15:45:15 +0530

    new project

--- ValidationOfEmail/src/com/EmailValidation.java
+++ ValidationOfEmail/src/com/EmailValidation.java
@@ -8,18 +8,19 @@ import java.util.regex.Pattern;
 public class EmailValidation {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String pattern ="[a-z,A-Z,0-9]+@[a-z]+\\.com";
-		System.out.println("Enter Email id :");
-		String name = sc.nextLine();
-		
-	//    String name ="a@b.com";
-		Pattern pt  = Pattern.compile(pattern);	
-		Matcher mt  = pt.matcher(name);		
-		if(mt.matches()) {
-			System.out.println("Valid Email");
-		}else {
-			System.out.println("Invalid Email");
+		try (Scanner sc = new Scanner(System.in)) {
+			String pattern ="[a-z,A-Z,0-9]+@[a-z]+\\.com";
+			System.out.println("Enter Email id :");
+			String name = sc.nextLine();
+			
+//    String name ="a@b.com";
+			Pattern pt  = Pattern.compile(pattern);	
+			Matcher mt  = pt.matcher(name);		
+			if(mt.matches()) {
+				System.out.println("Valid Email");
+			}else {
+				System.out.println("Invalid Email");
+			}
 		}
 
 	}
